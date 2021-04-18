9b30597e758b6d33f1cb635e5f09f0ce2f2b8357
jari <jaarhonka@yahoo.com>
2021-04-18 19:23:09 +0300

    test more

--- test/run.py
+++ test/run.py
@@ -10,7 +10,7 @@ VU.add_verification_components()
 SRC_PATH = Path(__file__).parent / "src"
 
 VU.add_library("lib").add_source_files(
-    [SRC_PATH / "*.vhd", SRC_PATH / "test" / "*.vhd"]
+    [SRC_PATH / "test" / "*.vhd"]
 )
 print([SRC_PATH / "*.vhd", SRC_PATH / "test" / "*.vhd"])
 
