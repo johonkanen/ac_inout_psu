ba36737f9c194b2de47fd0f882eb922fd1aff3e7
jari <jaarhonka@yahoo.com>
2021-05-21 09:14:43 +0300

    minor cleanup

--- source/system_control/system_control.vhd
+++ source/system_control/system_control.vhd
@@ -19,15 +19,32 @@ end entity system_control;
 
 architecture rtl of system_control is
 
+    alias core_clock is system_control_clocks.clock;
+    alias reset_n    is system_control_clocks.reset_n;
+
     signal system_components_clocks   : system_components_clock_group;
     signal system_components_data_in  : system_components_data_input_group;
     signal system_components_data_out : system_components_data_output_group;
     
 begin
 
-    system_components_clocks <=( clock => system_control_clocks.clock,
-                               reset_n =>system_control_clocks.reset_n);
+    main_system_controller : process(core_clock)
+        
+    begin
+        if rising_edge(core_clock) then
+            if reset_n = '0' then
+            -- reset state
+    
+            else
+    
+            end if; -- rstn
+        end if; --rising_edge
+    end process main_system_controller;	
+
 ------------------------------------------------------------------------
+    system_components_clocks <=( clock => system_control_clocks.clock,
+                               reset_n => system_control_clocks.reset_n);
+
     u_system_components : system_components
     port map( system_components_clocks,
     	  system_control_FPGA_in.system_components_FPGA_in,
@@ -35,4 +52,5 @@ begin
     	  system_components_data_in,
     	  system_components_data_out);
 
+------------------------------------------------------------------------
 end rtl;
