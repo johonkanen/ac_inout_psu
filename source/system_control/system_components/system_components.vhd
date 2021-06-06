00951c9fd44cd25a7bd29394229a2492b47aec82
jari <jaarhonka@yahoo.com>
2021-06-06 21:26:51 +0300

    savegame

--- source/system_control/system_components/system_components.vhd
+++ source/system_control/system_components/system_components.vhd
@@ -36,9 +36,7 @@ architecture rtl of system_components is
 
     signal uart_transmit_counter : natural range 0 to 2**16-1 := 0;
     constant counter_at_100khz   : natural                    := 120e6/100e3;
-    signal transmit_counter      : natural range 0 to 2**16-1 := 0;
 
-    signal toggle : std_logic := '0';
     signal uart_rx_data : natural range 0 to 2**16-1;
 
     signal spi_sar_adc_clocks   : spi_sar_adc_clock_group;
@@ -61,16 +59,35 @@ architecture rtl of system_components is
     signal filter_is_triggered : boolean;
     signal filter_counter : natural range 0 to 15 := 0;
 
+    signal test_counter : natural range 0 to 2**16-1;
+
+    function integer_to_std
+    (
+        number_to_be_converted : integer;
+        bits_in_word : integer
+    )
+    return std_logic_vector
+    is
+    begin
+        return std_logic_vector(to_unsigned(number_to_be_converted,bits_in_word));
+
+        
+    end integer_to_std;
+
+
+
 --------------------------------------------------
 begin
 
+--------------------------------------------------
     multiplier_clocks <= (clock => system_components_clocks.clock);
     u_multiplier : multiplier
-    port map( multiplier_clocks,
-    	  multiplier_data_in,
+    port map( multiplier_clocks ,
+    	  multiplier_data_in    ,
     	  multiplier_data_out); 
+
 --------------------------------------------------
-    test_uart : process(clock)
+    test_with_uart : process(clock)
         
     begin
         if rising_edge(clock) then
@@ -84,21 +101,22 @@ begin
                 uart_transmit_counter <= counter_at_100khz;
                 start_ad_conversion(spi_sar_adc_data_in);
 
-                transmit_counter <= transmit_counter - 1; 
-                if transmit_counter = 0 then
-                    transmit_counter <= uart_rx_data;
-                end if; 
-
             end if;
+
+            system_components_FPGA_out.test_ad_mux <= integer_to_std(number_to_be_converted => uart_rx_data, bits_in_word => 3);
             filter_is_triggered <= false;
             receive_data_from_uart(uart_data_out, uart_rx_data);
             if ad_conversion_is_ready(spi_sar_adc_data_out) then
+                test_counter <= test_counter + 1;
                 filter_is_triggered <= true;
                 adc_data <= get_adc_data(spi_sar_adc_data_out);
                 CASE uart_rx_data is
-                    WHEN 0 => transmit_16_bit_word_with_uart(uart_data_in, filter_output );
-                    WHEN 1 => transmit_16_bit_word_with_uart(uart_data_in, (adc_data + get_adc_data(spi_sar_adc_data_out))/2);
-                    WHEN others => transmit_16_bit_word_with_uart(uart_data_in, get_adc_data(spi_sar_adc_data_out));
+                    WHEN 10 => transmit_16_bit_word_with_uart(uart_data_in, filter_output );
+                    WHEN 11 => transmit_16_bit_word_with_uart(uart_data_in, (adc_data + get_adc_data(spi_sar_adc_data_out))/2);
+                    WHEN 12 => transmit_16_bit_word_with_uart(uart_data_in, get_adc_data(spi_sar_adc_data_out)); 
+                    WHEN 13 => transmit_16_bit_word_with_uart(uart_data_in, get_adc_data(spi_sar_adc_data_out));
+                    WHEN 14 => transmit_16_bit_word_with_uart(uart_data_in, test_counter);
+                    WHEN others =>  transmit_16_bit_word_with_uart(uart_data_in, uart_rx_data); 
                 end CASE;
             end if;
 
@@ -108,40 +126,52 @@ begin
 
             CASE filter_counter is
                 WHEN 0 =>
-                    multiply(multiplier_data_in, adc_data, b0);
+                    multiply(multiplier_data_in, test_counter, b0);
                     filter_counter <= filter_counter + 1;
+
                 WHEN 1 =>
-                    multiply(multiplier_data_in, adc_data, b1);
+                    multiply(multiplier_data_in, test_counter, b1);
                     filter_counter <= filter_counter + 1;
+
                 WHEN 2 =>
-                    if multiplier_is_ready(multiplier_data_out) then
-                        filter_output <= filter_memory + get_multiplier_result(multiplier_data_out, 17);
-                        filter_counter <= filter_counter + 1;
-                    end if;
+                    filter_counter <= filter_counter + 1;
+                    multiply(multiplier_data_in, 0, 0);
+
                 WHEN 3 =>
-                    filter_memory <= get_multiplier_result(multiplier_data_out, 17);
+                    filter_counter <= filter_counter + 1;
+
+                WHEN 4 =>
+                    filter_output <= filter_memory + get_multiplier_result(multiplier_data_out, 17);
                     multiply(multiplier_data_in, filter_output, a1);
                     filter_counter <= filter_counter + 1;
-                when 4 =>
-                    if multiplier_is_ready(multiplier_data_out) then
-                        filter_memory <= filter_memory + get_multiplier_result(multiplier_data_out, 17);
-                        filter_counter <= filter_counter + 1;
-                    end if;
+                    
+                WHEN 5 =>
+                    filter_memory <= get_multiplier_result(multiplier_data_out, 17);
+                    filter_counter <= filter_counter + 1;
+
+                WHEN 6 =>
+                    filter_counter <= filter_counter + 1;
+
+                WHEN 7 =>
+                    filter_counter <= filter_counter + 1;
+
+                when 8 =>
+                    filter_memory <= filter_memory + get_multiplier_result(multiplier_data_out, 17);
+                    filter_counter <= filter_counter + 1;
+
                 WHEN others => -- do nothing
-            end CASE;
-
-
+            end CASE; 
 
         end if; --rising_edge
-    end process test_uart;	
+    end process test_with_uart;	
 
 ------------------------------------------------------------------------ 
     spi_sar_adc_clocks <= (clock => system_components_clocks.clock, reset_n => reset_n); 
     u_spi_sar_adc : spi_sar_adc
-    port map( spi_sar_adc_clocks,
-          system_components_FPGA_in.spi_sar_adc_FPGA_in,
-    	  system_components_FPGA_out.spi_sar_adc_FPGA_out,
-    	  spi_sar_adc_data_in,
+    port map( spi_sar_adc_clocks                          ,
+          system_components_FPGA_in.spi_sar_adc_FPGA_in   ,
+    	  system_components_FPGA_out.spi_sar_adc_FPGA_out ,
+    	  spi_sar_adc_data_in                             ,
     	  spi_sar_adc_data_out);
 
 ------------------------------------------------------------------------ 
