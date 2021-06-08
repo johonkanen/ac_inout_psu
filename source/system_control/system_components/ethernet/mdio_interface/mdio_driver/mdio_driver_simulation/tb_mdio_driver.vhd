6f11f81e701135a5991431ce50e21f73b0f80d29
jari <jaarhonka@yahoo.com>
2021-06-09 01:38:31 +0300

    write data with mdio

--- source/system_control/system_components/ethernet/mdio_interface/mdio_driver/mdio_driver_simulation/tb_mdio_driver.vhd
+++ source/system_control/system_components/ethernet/mdio_interface/mdio_driver/mdio_driver_simulation/tb_mdio_driver.vhd
@@ -30,17 +30,11 @@ architecture sim of tb_mdio_driver is
 
     signal simulator_counter          : natural := 0;
     signal data_from_mdio             : std_logic_vector(15 downto 0);
-    signal mdio_data_out              : std_logic;
-    signal mdio_clock_out             : std_logic;
     signal mdio_data_output_reference : std_logic;
 
 
-    signal mdio_transmit_register : std_logic_vector(15 downto 0) := x"aaaa";
-    signal mdio_receive_shift_register  : std_logic_vector(15 downto 0);
-    signal mdio_clock : std_logic;
+    signal mdio_receive_shift_register  : std_logic_vector(33 downto 0) := (others => '0');
     signal mdio_clock_buffer : std_logic;
-    signal mdio_clock_counter : natural range 0 to 15 := 0;
-    constant mdio_clock_divisor_counter_high : integer := 4;
 
 begin
 
@@ -91,32 +85,16 @@ begin
         if rising_edge(simulator_clock) then
 
             simulator_counter <= simulator_counter + 1;
-            mdio_clock_counter <= mdio_clock_counter + 1;
-            if mdio_clock_counter = mdio_clock_divisor_counter_high then 
-                mdio_clock_counter <= 0;
-            end if;
 
-            mdio_clock <= '1';
-            if mdio_clock_counter > mdio_clock_divisor_counter_high/2-1 then
-                mdio_clock <= '0'; 
-            end if; 
-
-            if mdio_clock_counter = mdio_clock_divisor_counter_high/2-1 then
-                mdio_transmit_register <= mdio_transmit_register(mdio_transmit_register'left-1 downto 0) & '0';
+            init_mdio_driver(mdio_driver_data_in);
+            if simulator_counter = 10 then
+                write_data_to_mdio(mdio_driver_data_in, x"0f", x"0f", x"acdc");
             end if;
 
             mdio_clock_buffer <= mdio_driver_FPGA_out.mdio_clock;
             if mdio_clock_buffer = '1' and mdio_driver_FPGA_out.mdio_clock = '0' then
                 mdio_receive_shift_register <= mdio_receive_shift_register(mdio_receive_shift_register'left-1 downto 0) & mdio_driver_FPGA_out.MDIO_serial_data_out;
             end if;
-
-            CASE simulator_counter is
-                WHEN 82 => 
-                    mdio_transmit_register <= x"acdc";
-                WHEN others =>
-            end CASE;
-
-
 
         end if; --rising_edge
     end process test_mdio_driver;	
