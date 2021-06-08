0cb1f6fa36636936af1832e633d96552a9d0f65e
jari <jaarhonka@yahoo.com>
2021-06-09 00:48:36 +0300

    start designing mdio driver

--- source/system_control/system_components/ethernet/mdio_interface/mdio_driver/mdio_driver.vhd
+++ source/system_control/system_components/ethernet/mdio_interface/mdio_driver/mdio_driver.vhd
@@ -10,9 +10,7 @@ entity mdio_driver is
     port (
         mdio_driver_clocks : in mdio_driver_clock_group;
 
-        mdio_driver_FPGA_in  : in mdio_driver_FPGA_input_group;
-        mdio_driver_FPGA_out : out mdio_driver_FPGA_output_group;
-
+        mdio_driver_FPGA_out : out mdio_driver_FPGA_output_group; 
         mdio_driver_data_in  : in mdio_driver_data_input_group;
         mdio_driver_data_out : out mdio_driver_data_output_group
     );
@@ -20,191 +18,65 @@ end mdio_driver;
 
 architecture rtl of mdio_driver is
 
-    alias mdio_data_read_is_requested is mdio_driver_data_in.mdio_data_read_is_requested;
-    alias mdio_data_write_is_requested is mdio_driver_data_in.mdio_data_write_is_requested;
-    alias phy_address is mdio_driver_data_in.phy_address;
-    alias phy_register_address is mdio_driver_data_in.phy_register_address;
     alias core_clock is mdio_driver_clocks.clock;
-    alias reset_n is mdio_driver_clocks.reset_n;
-
-    signal mdio_clock : std_logic;
 
     constant MDIO_write_command : std_logic_vector(5 downto 0) := "110101";
     constant MDIO_read_command  : std_logic_vector(5 downto 0) := "110110";
 
-    signal mdio_transmit_shift_register : std_logic_vector(15 downto 0);
     signal mdio_receive_shift_register  : std_logic_vector(15 downto 0);
 
-    signal bit_counter : integer range 0 to 2**8-1;
-    signal clock_divisor_counter : integer range 0 to 7;
+    constant mdio_clock_divisor_counter_high : integer := 4;
 
-    signal falling_edge_counter : integer := 0;
-    constant number_of_clocks_in_bit : integer := 5;
-    signal mdio_requested : boolean;
+    type mdio_transmit_control_group is record
+        mdio_transmit_register          : std_logic_vector(15 downto 0);
+        mdio_clock                      : std_logic;
+        mdio_clock_counter              : natural range 0 to 15;
+        MDIO_io_direction_is_out_when_1 : std_logic;
+    end record;
+
+    constant mdio_transmit_control_init : mdio_transmit_control_group := (x"acdc",'0',0,'1');
+    signal mdio_transmit_control : mdio_transmit_control_group := mdio_transmit_control_init;
 
 begin
 
-    mdio_driver_FPGA_out.MDIO_serial_data_out <= mdio_transmit_shift_register(15);
-    mdio_driver_FPGA_out.mdio_clock <= mdio_clock;
+    mdio_driver_FPGA_out <= ( 
+                            MDIO_serial_data_out            => mdio_transmit_control.mdio_transmit_register(15) ,
+                            MDIO_io_direction_is_out_when_1 => '1'                                              ,
+                            MDIO_clock                      => mdio_transmit_control.mdio_clock);
 
 ------------------------------------------------------------------------
     mdio_io_driver : process(core_clock)
 
     --------------------------------------------------
-        type list_of_mdio_states is (idle, send_phy_read_command, send_read_command_address, read_data, send_phy_write_command, send_write_command_address, write_data);
+        type list_of_mdio_states is (idle, transmit_command, transmit_address);
         variable mdio_state : list_of_mdio_states;
 
-        type t_mdio_states is (idle, send_phy_read_command, send_read_command_address, read_data, 
-                                    send_phy_write_command, send_write_command_address, write_data);
+        procedure generate_mdio_io_waveforms
+        (
+            signal mdio_transmit : inout mdio_transmit_control_group
+        ) is
+        begin
 
-        variable st_mdio_states : t_mdio_states;
-        variable mdio_command : std_logic_vector(5 downto 0);
+            mdio_transmit.mdio_clock_counter <= mdio_transmit.mdio_clock_counter + 1;
+            if mdio_transmit.mdio_clock_counter = mdio_clock_divisor_counter_high then 
+                mdio_transmit.mdio_clock_counter <= 0;
+            end if;
+
+            mdio_transmit.mdio_clock <= '1';
+            if mdio_transmit.mdio_clock_counter > mdio_clock_divisor_counter_high/2-1 then
+                mdio_transmit.mdio_clock <= '0'; 
+            end if; 
+
+            if mdio_transmit.mdio_clock_counter = mdio_clock_divisor_counter_high/2-2 then
+                mdio_transmit.mdio_transmit_register <= mdio_transmit.mdio_transmit_register(mdio_transmit.mdio_transmit_register'left-1 downto 0) & '0';
+            end if;
+            
+        end generate_mdio_io_waveforms;
+
     --------------------------------------------------
     begin
         if rising_edge(core_clock) then
-            if reset_n = '0' then
-            -- reset state
-                mdio_clock <='0'; 
-                mdio_transmit_shift_register <= x"ffff";
-                mdio_receive_shift_register <= x"0000";
-                clock_divisor_counter <= 0;
-                mdio_requested <= false;
-                mdio_driver_FPGA_out.MDIO_io_direction_1_output <= '0';
-                -- set_mdio_direction_to_write(mdio_driver_FPGA_out);
-                mdio_command := MDIO_read_command;
-            else
-
-                --- create 25 MHz mdio clock -----
-                clock_divisor_counter <= clock_divisor_counter + 1;
-                if clock_divisor_counter = number_of_clocks_in_bit-1 then
-                    clock_divisor_counter <= 0;
-                end if;
-
-                mdio_clock <= '1';
-                if clock_divisor_counter > number_of_clocks_in_bit/2 then
-                    mdio_clock <= '0';
-                end if;
-
-                --------------------- mdio output register -----------------------------
-                if clock_divisor_counter = 3 then
-                    mdio_transmit_shift_register <= mdio_transmit_shift_register(14 downto 0) & '1';
-                end if;
-
-                --------------------- mdio input register -----------------------------
-                if clock_divisor_counter = 3 then
-                    mdio_receive_shift_register    <= mdio_receive_shift_register(14 downto 0) & '0';
-                    mdio_receive_shift_register(0) <= mdio_driver_FPGA_in.MDIO_serial_data_in;
-                end if;
-
-                mdio_driver_FPGA_out.MDIO_io_direction_1_output <= '0';
-                mdio_driver_data_out.mdio_driver_is_ready <= false;
-                --------------------- main state machine for mdio io control ---------
-                CASE st_mdio_states is 
-                    WHEN idle =>
-                        -- wait for command
-
-                        bit_counter <= 0;
-
-                        st_mdio_states := idle;
-                        if mdio_data_read_is_requested then
-                            mdio_requested <= true;
-                            mdio_command := MDIO_read_command;
-                        end if;
-
-                        if mdio_data_write_is_requested then
-                            mdio_requested <= true;
-                            mdio_command := MDIO_write_command;
-                        end if;
-
-                        if mdio_requested AND clock_divisor_counter = 3 then
-                            mdio_requested <= false;
-                            mdio_driver_FPGA_out.MDIO_io_direction_1_output <= '1';
-                            mdio_transmit_shift_register(15 downto 15-5) <= mdio_command;
-                            falling_edge_counter <= 1;
-                            if mdio_command = MDIO_read_command then
-                                st_mdio_states := send_phy_read_command;
-                            else
-                                st_mdio_states := send_phy_write_command;
-                            end if;
-                        end if;
-
-                ---------------- read branch --------------------     
-                    WHEN send_phy_read_command =>
-                            mdio_driver_FPGA_out.MDIO_io_direction_1_output <= '1';
-
-                            bit_counter <= bit_counter + 1;
-
-                        st_mdio_states := send_phy_read_command;
-                        if bit_counter = 29 then
-                            transmit_phy_command_address(mdio_transmit_shift_register, phy_address, phy_register_address);
-                            st_mdio_states := send_read_command_address;
-                            bit_counter <= 0;
-                            falling_edge_counter <= 0;
-                        end if;
-
-                    WHEN send_read_command_address =>
-                            mdio_driver_FPGA_out.MDIO_io_direction_1_output <= '1';
-
-                        bit_counter <= bit_counter + 1;
-
-                        if bit_counter = 58 then
-                            bit_counter <= 0;
-                            st_mdio_states := read_data;
-                            mdio_driver_FPGA_out.MDIO_io_direction_1_output <= '0';
-
-                        end if;
-
-                    WHEN read_data =>
-                        mdio_driver_FPGA_out.MDIO_io_direction_1_output <= '0';
-
-                        bit_counter <= bit_counter + 1;
-                        if bit_counter = 78 then
-                            bit_counter <= 0;
-                            st_mdio_states := idle;
-                            mdio_driver_data_out.data_from_mdio <= mdio_receive_shift_register;
-                            mdio_driver_data_out.mdio_driver_is_ready <= true;
-                        end if;
-
-                ---------------- write branch --------------------     
-                    WHEN send_phy_write_command =>
-                            mdio_driver_FPGA_out.MDIO_io_direction_1_output <= '1';
-
-                            bit_counter <= bit_counter + 1;
-
-                        st_mdio_states := send_phy_write_command;
-                        if bit_counter = 29 then
-                            transmit_phy_command_address(mdio_transmit_shift_register, phy_address, phy_register_address);
-                            st_mdio_states := send_write_command_address;
-                            bit_counter <= 0;
-                            falling_edge_counter <= 0;
-                        end if;
-
-                    WHEN send_write_command_address =>
-                            mdio_driver_FPGA_out.MDIO_io_direction_1_output <= '1';
-
-                        bit_counter <= bit_counter + 1;
-
-                        st_mdio_states := send_write_command_address;
-                        if bit_counter = 60 then
-                            bit_counter <= 0;
-                            st_mdio_states := write_data;
-                            mdio_transmit_shift_register <= mdio_driver_data_in.data_to_mdio;
-                        end if;
-
-                    WHEN write_data =>
-                        mdio_driver_FPGA_out.MDIO_io_direction_1_output <= '1';
-
-                        st_mdio_states := write_data;
-                        bit_counter <= bit_counter + 1;
-                        if bit_counter = 78 then
-                            bit_counter <= 0;
-                            st_mdio_states := idle;
-                            mdio_driver_data_out.mdio_driver_is_ready <= true;
-                        end if;
-
-
-                end CASE;
-            end if; -- rstn
+            generate_mdio_io_waveforms(mdio_transmit_control);
         end if; --rising_edge
     end process mdio_io_driver;	
 
--- source/system_control/system_components/ethernet/mdio_interface/mdio_driver/mdio_driver_simulation/tb_mdio_driver.vhd
+++ source/system_control/system_components/ethernet/mdio_interface/mdio_driver/mdio_driver_simulation/tb_mdio_driver.vhd
@@ -16,7 +16,7 @@ architecture sim of tb_mdio_driver is
     signal rstn : std_logic;
 
     signal simulation_running  : boolean;
-    signal simulator_clock     : std_logic;
+    signal simulator_clock     : std_logic :='0';
     signal clocked_reset       : std_logic;
     constant clock_per         : time       := 1 ns;
     constant clock_half_per    : time       := 0.5 ns;
@@ -34,6 +34,13 @@ architecture sim of tb_mdio_driver is
     signal mdio_clock_out             : std_logic;
     signal mdio_data_output_reference : std_logic;
 
+
+    signal mdio_transmit_register : std_logic_vector(15 downto 0) := x"aaaa";
+    signal mdio_receive_shift_register  : std_logic_vector(15 downto 0);
+    signal mdio_clock : std_logic;
+    signal mdio_clock_buffer : std_logic;
+    signal mdio_clock_counter : natural range 0 to 15 := 0;
+    constant mdio_clock_divisor_counter_high : integer := 4;
 
 begin
 
@@ -77,59 +84,40 @@ begin
 ------------------------------------------------------------------------
     test_mdio_driver : process(simulator_clock)
         constant reference_data_offset : integer := -4;
+
+
         
     begin
         if rising_edge(simulator_clock) then
-            if clocked_reset = '0' then
-            -- reset state
-                init_mdio_driver(mdio_driver_data_in);
-                simulator_counter <= 0;
-    
-            else
-                CASE simulator_counter is
-                    WHEN 0 =>
-                        mdio_data_output_reference <= '0';
-                    WHEN 12 =>
-                        mdio_data_output_reference <= '1';
-                    WHEN 22 =>
-                        mdio_data_output_reference <= '0';
-                    WHEN 27 =>
-                        mdio_data_output_reference <= '1';
-                    WHEN 37 =>
-                        mdio_data_output_reference <= '0';
-                    WHEN 42 =>
-                        mdio_data_output_reference <= '1';
-                    WHEN 67 =>
-                        mdio_data_output_reference <= '0';
-                    WHEN 92 =>
-                        mdio_data_output_reference <= '1';
-                    WHEN 97 =>
-                        mdio_data_output_reference <= '0';
-                    WHEN 102 =>
-                        -- set directio to input
-                        mdio_data_output_reference <= '1';
-                    WHEN 182 =>
-                        -- set directio to output
-                        mdio_data_output_reference <= '0';
-                    when others => -- do nothing
-                end CASE;
 
-                simulator_counter <= simulator_counter + 1;
-                init_mdio_driver(mdio_driver_data_in);
+            simulator_counter <= simulator_counter + 1;
+            mdio_clock_counter <= mdio_clock_counter + 1;
+            if mdio_clock_counter = mdio_clock_divisor_counter_high then 
+                mdio_clock_counter <= 0;
+            end if;
 
-                init_mdio_driver(mdio_driver_data_in);
-                CASE simulator_counter is
-                    WHEN 4 => 
-                        read_data_from_mdio(mdio_driver_data_in,x"f0",x"0f");
-                    when others =>
-                        -- do nothing 
-                end CASE;
+            mdio_clock <= '1';
+            if mdio_clock_counter > mdio_clock_divisor_counter_high/2-1 then
+                mdio_clock <= '0'; 
+            end if; 
 
-                if mdio_is_ready(mdio_driver_data_out) then
-                    data_from_mdio <= get_data_from_mdio(mdio_driver_data_out);
-                end if;
-    
-            end if; -- rstn
+            if mdio_clock_counter = mdio_clock_divisor_counter_high/2-1 then
+                mdio_transmit_register <= mdio_transmit_register(mdio_transmit_register'left-1 downto 0) & '0';
+            end if;
+
+            mdio_clock_buffer <= mdio_driver_FPGA_out.mdio_clock;
+            if mdio_clock_buffer = '1' and mdio_driver_FPGA_out.mdio_clock = '0' then
+                mdio_receive_shift_register <= mdio_receive_shift_register(mdio_receive_shift_register'left-1 downto 0) & mdio_driver_FPGA_out.MDIO_serial_data_out;
+            end if;
+
+            CASE simulator_counter is
+                WHEN 82 => 
+                    mdio_transmit_register <= x"acdc";
+                WHEN others =>
+            end CASE;
+
+
+
         end if; --rising_edge
     end process test_mdio_driver;	
 
@@ -137,14 +125,9 @@ begin
     mdio_driver_clocks <= (clock => simulator_clock,
                           reset_n => clocked_reset);
 
-    mdio_driver_FPGA_in.MDIO_serial_data_in <= mdio_driver_FPGA_out.MDIO_serial_data_out;
-    mdio_data_out                           <= mdio_driver_FPGA_out.MDIO_serial_data_out;
-    mdio_clock_out                          <= mdio_driver_FPGA_out.mdio_clock;
-
     u_mdio_driver : mdio_driver
     port map(
         mdio_driver_clocks   ,
-        mdio_driver_FPGA_in  , -- route out of fpga
         mdio_driver_FPGA_out ,
         mdio_driver_data_in  ,
         mdio_driver_data_out);
