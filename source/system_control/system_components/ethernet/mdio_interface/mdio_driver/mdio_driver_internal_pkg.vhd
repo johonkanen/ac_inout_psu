53648d683ec83591e67db4c9264c33561fbde6d7
jari <jaarhonka@yahoo.com>
2021-06-09 07:35:47 +0300

    add write is ready to mdio

--- source/system_control/system_components/ethernet/mdio_interface/mdio_driver/mdio_driver_internal_pkg.vhd
+++ source/system_control/system_components/ethernet/mdio_interface/mdio_driver/mdio_driver_internal_pkg.vhd
@@ -8,23 +8,27 @@ library work;
 package mdio_driver_internal_pkg is
 
     constant MDIO_write_command : std_logic_vector(5 downto 0) := "110101";
+    constant MDIO_write_data_delimiter  : std_logic_vector(1 downto 0) := "10";
     constant MDIO_read_command  : std_logic_vector(5 downto 0) := "110110";
 
     constant mdio_clock_divisor_counter_high : integer := 4;
+    constant mdio_transmit_counter_high : integer := (mdio_clock_divisor_counter_high+1)*33;
 
     type mdio_transmit_control_group is record
         mdio_transmit_register          : std_logic_vector(33 downto 0);
         mdio_clock                      : std_logic;
         mdio_clock_counter              : natural range 0 to 15;
         MDIO_io_direction_is_out_when_1 : std_logic;
+        mdio_transmit_clock : natural range 0 to 511;
+        mdio_transmit_is_ready : boolean;
     end record; 
-    constant mdio_transmit_control_init : mdio_transmit_control_group := ((others=>'0') ,'0',0,'1');
+    constant mdio_transmit_control_init : mdio_transmit_control_group := ((others=>'0') , '0' , 0 , '0' , 0, false);
 
 --------------------------------------------------
     procedure generate_mdio_io_waveforms (
         signal mdio_transmit : inout mdio_transmit_control_group);
 --------------------------------------------------
-    procedure write_data_to_mdio (
+    procedure load_data_to_mdio_transmit_shift_register (
         signal mdio_control : out mdio_transmit_control_group;
         data : std_logic_vector );
 --------------------------------------------------
@@ -56,25 +60,20 @@ package body mdio_driver_internal_pkg is
         if mdio_transmit.mdio_clock_counter = mdio_clock_divisor_counter_high/2-2 then
             mdio_transmit.mdio_transmit_register <= mdio_transmit.mdio_transmit_register(mdio_transmit.mdio_transmit_register'left-1 downto 0) & '0';
         end if;
-        
-    end generate_mdio_io_waveforms;
---------------------------------------------------
-    procedure write_data_with_mdio
-    (
-        mdio_input : in mdio_driver_data_input_group;
-        signal mdio_control : out mdio_transmit_control_group
-    ) is
-    begin
-        if mdio_input.mdio_data_write_is_requested then
-            write_data_to_mdio(mdio_control, MDIO_write_command    &
-                                mdio_input.phy_address(4 downto 0)          &
-                                mdio_input.phy_register_address(4 downto 0) &
-                                mdio_input.data_to_mdio(15 downto 0));
+
+        if mdio_transmit.mdio_transmit_clock /= 0 then
+            mdio_transmit.mdio_transmit_clock <= mdio_transmit.mdio_transmit_clock - 1;
         end if;
 
-    end write_data_with_mdio;
+        mdio_transmit.mdio_transmit_is_ready <= false;
+        if mdio_transmit.mdio_transmit_clock = 1 then
+            mdio_transmit.mdio_transmit_is_ready <= true;
+        end if;
+        
+    end generate_mdio_io_waveforms;
+
 --------------------------------------------------
-    procedure write_data_to_mdio
+    procedure load_data_to_mdio_transmit_shift_register
     (
         signal mdio_control : out mdio_transmit_control_group;
         data : std_logic_vector
@@ -83,9 +82,27 @@ package body mdio_driver_internal_pkg is
     begin
         mdio_control.mdio_transmit_register(mdio_control.mdio_transmit_register'left downto mdio_control.mdio_transmit_register'left-data'high) <= data;
         
-    end write_data_to_mdio;
+    end load_data_to_mdio_transmit_shift_register;
+
 --------------------------------------------------
+    procedure write_data_with_mdio
+    (
+        mdio_input : in mdio_driver_data_input_group;
+        signal mdio_control : out mdio_transmit_control_group
+    ) is
+    begin
+        if mdio_input.mdio_data_write_is_requested then
+            load_data_to_mdio_transmit_shift_register(mdio_control, 
+                                MDIO_write_command                          &
+                                mdio_input.phy_address(4 downto 0)          &
+                                mdio_input.phy_register_address(4 downto 0) &
+                                "10"                                        &
+                                mdio_input.data_to_mdio(15 downto 0));
+            mdio_control.mdio_transmit_clock <= mdio_transmit_counter_high;
+        end if;
 
+    end write_data_with_mdio;
 
+--------------------------------------------------
 end package body mdio_driver_internal_pkg;
 
