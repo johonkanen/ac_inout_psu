e3a9cf65b380319e98af8c884b192d7e7e3cabcb
jari <jaarhonka@yahoo.com>
2021-07-03 01:17:19 +0300

    remove mmd layer

--- source/system_control/system_components/ethernet/ethernet.vhd
+++ source/system_control/system_components/ethernet/ethernet.vhd
@@ -5,7 +5,6 @@ library ieee;
 
 library work;
     use work.ethernet_pkg.all;
-    use work.mdio_mmd_access_control_pkg.all;
 
 entity ethernet is
     port (
@@ -22,24 +21,10 @@ architecture rtl of ethernet is
 
 --------------------------------------------------
 
-    signal mdio_mmd_access_control_clocks   : mdio_mmd_access_control_clock_group;
-    signal mdio_mmd_access_control_data_out : mdio_mmd_access_control_data_output_group;
-
 begin
 
-    ethernet_data_out <= (mdio_mmd_access_control_data_out => mdio_mmd_access_control_data_out);
 
 ------------------------------------------------------------------------
-    mdio_mmd_access_control_clocks <= (clock   => ethernet_clocks.core_clock,
-                              reset_n => '1');
-
-    u_mdio_mmd_access_control : mdio_mmd_access_control
-    port map(
-        mdio_mmd_access_control_clocks,
-        ethernet_FPGA_out.mdio_mmd_access_control_FPGA_out,
-        ethernet_FPGA_inout.mdio_mmd_access_control_FPGA_inout,
-        ethernet_data_in.mdio_mmd_access_control_data_in, 
-        mdio_mmd_access_control_data_out);
 
 ------------------------------------------------------------------------
 end rtl;
