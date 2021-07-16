3ca1ae2101af47deb0ee48e74cdfb86b0654276b
jari <jaarhonka@yahoo.com>
2021-07-16 21:00:13 +0300

    move ram controller implementation to ethernet fram ram read pkg

--- source/system_control/system_components/ethernet/ethernet_common/dual_port_ethernet_ram/ethernet_frame_ram_read_pkg.vhd
+++ source/system_control/system_components/ethernet/ethernet_common/dual_port_ethernet_ram/ethernet_frame_ram_read_pkg.vhd
@@ -36,7 +36,31 @@ package ethernet_frame_ram_read_pkg is
         ram_output : in ram_read_output_group;
         signal ram_shift_register : inout std_logic_vector);
 
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+    type ram_reader is record
+        transmit_counter : natural range 0 to 7;
+        ram_read_address : natural range 0 to 7;
+        ram_buffering_is_complete : boolean;
+        ram_address_buffer_counter : natural range 0 to 7;
+        ram_offset : natural range 0 to 2**11-1;
+    end record;
+------------------------------------------------------------------------
+    procedure create_ram_read_controller (
+        signal ram_read_port : out ram_read_control_group;
+        ram_output_port : in ram_read_output_group;
+        signal ram_controller : inout ram_reader;
+        signal ram_shift_register : inout std_logic_vector);
+------------------------------------------------------------------------
+    procedure load_ram_with_offset_to_shift_register (
+        signal ram_controller : inout  ram_reader;
+        start_address : natural;
+        number_of_ram_addresses_to_be_read : natural);
+------------------------------------------------------------------------
+    function ram_is_buffered_to_shift_register ( ram_controller : ram_reader)
+        return boolean;
 
+------------------------------------------------------------------------
 end package ethernet_frame_ram_read_pkg;
 
 
@@ -108,6 +132,62 @@ package body ethernet_frame_ram_read_pkg is
         end if;
 
     end load_ram_to_shift_register;
+
+------------------------------------------------------------------------
+    procedure create_ram_read_controller
+    (
+        signal ram_read_port : out ram_read_control_group;
+        ram_output_port : in ram_read_output_group;
+        signal ram_controller : inout ram_reader;
+        signal ram_shift_register : inout std_logic_vector
+    ) is
+    begin
+        init_ram_read(ram_read_port);
+        load_ram_to_shift_register(ram_output_port, ram_shift_register);
+
+        if ram_controller.ram_read_address > 0 then
+            ram_controller.ram_read_address <= ram_controller.ram_read_address - 1;
+            read_data_from_ram(ram_read_port, ram_controller.ram_offset+ram_controller.ram_read_address-1);
+        end if;
+
+        if ram_data_is_ready(ram_output_port) then
+            ram_controller.ram_address_buffer_counter <= ram_controller.ram_address_buffer_counter + 1;
+        end if;
+
+        ram_controller.ram_buffering_is_complete <= false;
+        if ram_controller.ram_address_buffer_counter = 1 then
+            ram_controller.ram_buffering_is_complete <= true;
+        end if;
+        
+    end create_ram_read_controller; 
+
+------------------------------------------------------------------------
+    procedure load_ram_with_offset_to_shift_register
+    (
+        signal ram_controller : inout  ram_reader;
+        start_address : natural;
+        number_of_ram_addresses_to_be_read : natural
+    ) is
+    begin
+        ram_controller.ram_read_address <= number_of_ram_addresses_to_be_read;
+        ram_controller.ram_address_buffer_counter <= 0;
+        ram_controller.ram_offset <= start_address; 
+
+    end load_ram_with_offset_to_shift_register;
+------------------------------------------------------------------------
+    function ram_is_buffered_to_shift_register
+    (
+        ram_controller : ram_reader
+    )
+    return boolean
+    is
+    begin
+       return ram_controller.ram_buffering_is_complete; 
+    end ram_is_buffered_to_shift_register;
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+
+
 
 end package body ethernet_frame_ram_read_pkg;
 
