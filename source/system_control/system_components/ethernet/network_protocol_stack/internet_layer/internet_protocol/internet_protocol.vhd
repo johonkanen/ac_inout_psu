9f378b87c9ede707269e8bd35ded25e70b5da0bc
jari <jaarhonka@yahoo.com>
2021-07-17 18:30:22 +0300

    remove ethernet specific header

--- source/system_control/system_components/ethernet/network_protocol_stack/internet_layer/internet_protocol/internet_protocol.vhd
+++ source/system_control/system_components/ethernet/network_protocol_stack/internet_layer/internet_protocol/internet_protocol.vhd
@@ -2,26 +2,27 @@ library ieee;
     use ieee.std_logic_1164.all;
     use ieee.numeric_std.all;
 
-library work;
-    use work.ethernet_frame_ram_read_pkg.all;
-    use work.internet_protocol_pkg.all;
+-- entity network_protocol is
+--     port (
+--         network_protocol_clocks   : in network_protocol_clock_group;
+--         network_protocol_data_in  : in network_protocol_data_input_group;
+--         network_protocol_data_out : out network_protocol_data_output_group
+--     );
+-- end entity network_protocol;
 
-entity internet_protocol is
-    port (
-        internet_protocol_clocks : in internet_protocol_clock_group; 
-        internet_protocol_data_in : in internet_protocol_data_input_group;
-        internet_protocol_data_out : out internet_protocol_data_output_group
-    );
-end entity internet_protocol;
+architecture internet_protocol of network_protocol is
 
-architecture rtl of internet_protocol is
+    alias clock is network_protocol_clocks.clock;
+    alias internet_protocol_data_in is network_protocol_data_in;
+    alias internet_protocol_data_out is network_protocol_data_out;
+    alias protocol_control is internet_protocol_data_in.protocol_control; 
 
-    alias clock is internet_protocol_clocks.clock;
     signal frame_ram_read_control_port : ram_read_control_group;
     signal shift_register : std_logic_vector(31 downto 0);
     signal ram_read_controller : ram_reader;
     signal ram_offset : natural range 0 to 2**11-1;
-    
+    signal header_offset : natural range 0 to 2**11-1;
+
 begin
 
 ------------------------------------------------------------------------
@@ -41,7 +42,19 @@ begin
         if rising_edge(clock) then
             create_ram_read_controller(frame_ram_read_control_port, internet_protocol_data_in.frame_ram_output, ram_read_controller, shift_register); 
 
+            if protocol_control.protocol_processing_is_requested then
+                header_offset <= protocol_control.protocol_start_address;
+            end if;
+
+                if get_ram_address(internet_protocol_data_in.frame_ram_output) = header_offset+8 then
+                    if shift_register(7 downto 0) = x"11" then
+                        ram_offset <= 14 + 8;
+                    else
+                        ram_offset <= 0;
+                    end if;
+                end if;
+
         end if; --rising_edge
     end process ip_header_processor;	
 
-end rtl;
+end internet_protocol;
