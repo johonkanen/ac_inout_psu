03e5fad0f459c838f0fe59c01c661eb8f4894e0f
jari <jaarhonka@yahoo.com>
2021-07-29 13:57:22 +0300

    start adding scripts for common build

---
+++ cyclone_10/tcl/control_card_pin_map.tcl
@@ -0,0 +1,39 @@
+
+set main_clock PIN_M2 
+
+set uart_rx PIN_T6
+set uart_tx PIN_R6
+
+set gate_drive_pwm_0 PIN_R13
+set gate_drive_pwm_1 PIN_T12 
+set gate_drive_pwm_2 PIN_R12 
+set gate_drive_pwm_3 PIN_T5  
+set gate_drive_pwm_4 PIN_L1  
+set gate_drive_pwm_5 PIN_R4  
+
+set spi_serial_data PIN_T2
+set chip_select PIN_R1
+set spi_clock PIN_P2
+
+set test_ad_mux_0 PIN_L16
+set test_ad_mux_1 PIN_L15
+set test_ad_mux_2 PIN_N15
+
+set MDIO_inout_data PIN_A5
+set mdio_clock PIN_B4 
+
+set ethernet_tx_ddr_clock PIN_B14
+
+set tx_ctl PIN_C16
+set rgmii_tx_0 PIN_C14
+set rgmii_tx_1 PIN_B16
+set rgmii_tx_2 PIN_A15
+set rgmii_tx_3 PIN_A14
+
+set enet_ref_clk_125MHz PIN_B8
+set rx_ctl PIN_B5
+
+set ethernet_rx_ddio_in_0 PIN_A6
+set ethernet_rx_ddio_in_1 PIN_B6
+set ethernet_rx_ddio_in_2 PIN_A7
+set ethernet_rx_ddio_in_3 PIN_B7
