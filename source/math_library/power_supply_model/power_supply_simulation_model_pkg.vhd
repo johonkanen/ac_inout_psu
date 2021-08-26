0648530713243f33b8bbd718c54ef8294f568194
jari <jaarhonka@yahoo.com>
2021-08-26 20:05:53 +0300

    clean up system model simulation

--- source/math_library/power_supply_model/power_supply_simulation_model_pkg.vhd
+++ source/math_library/power_supply_model/power_supply_simulation_model_pkg.vhd
@@ -20,9 +20,15 @@ package power_supply_simulation_model_pkg is
 
     constant power_supply_model_init : power_supply_model_record := (grid_inverter_init, output_inverter_init, pi_controller_init, multiplier_init_values);
 
+    --------------------------------------------------
     procedure create_power_supply_simulation_model (
         signal power_supply_simulation : inout power_supply_model_record;
         output_inverter_load_current : int18);
+    --------------------------------------------------
+    procedure request_power_supply_calculation (
+        signal power_supply_simulation : inout power_supply_model_record;
+        grid_inverter_duty_ratio       : int18;
+        output_inverter_duty_ratio     : int18);
 ------------------------------------------------------------------------
 
 end package power_supply_simulation_model_pkg;
@@ -46,6 +52,25 @@ package body power_supply_simulation_model_pkg is
         create_output_inverter(output_inverter_simulation, dab_pi_controller.pi_out, output_inverter_load_current);
         
     end create_power_supply_simulation_model;
+
+    --------------------------------------------------
+    procedure request_power_supply_calculation
+    (
+        signal power_supply_simulation : inout power_supply_model_record;
+        grid_inverter_duty_ratio : int18;
+        output_inverter_duty_ratio : int18
+    ) is
+        alias grid_inverter_simulation is power_supply_simulation.grid_inverter_simulation;
+        alias output_inverter_simulation is power_supply_simulation.output_inverter_simulation;
+        alias dab_pi_controller is power_supply_simulation.dab_pi_controller;
+        alias multiplier is power_supply_simulation.multiplier;
+    begin
+
+        calculate_pi_control(dab_pi_controller, output_inverter_simulation.output_inverter.dc_link_voltage - grid_inverter_simulation.grid_inverter.dc_link_voltage); 
+        request_grid_inverter_calculation(grid_inverter_simulation, grid_inverter_duty_ratio); 
+        request_output_inverter_calculation(output_inverter_simulation, output_inverter_duty_ratio); 
+        
+    end request_power_supply_calculation;
 
 ------------------------------------------------------------------------ 
 end package body power_supply_simulation_model_pkg; 
