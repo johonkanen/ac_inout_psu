da72d782fdedaa7e69e8b3520703a71bf2943007
jari <jaarhonka@yahoo.com>
2021-09-18 17:15:52 +0300

    add division sources to grid inverter simulation

--- source/math_library/grid_inverter_control/simulate_grid_inverter_control/sim_grid_inverter_control.bat
+++ source/math_library/grid_inverter_control/simulate_grid_inverter_control/sim_grid_inverter_control.bat
@@ -8,6 +8,14 @@ SET source=%project_root%/source
 
 ghdl -a --ieee=synopsys --work=math_library %source%/math_library/multiplier/multiplier_pkg.vhd
 ghdl -a --ieee=synopsys --work=math_library %source%/math_library/sincos/sincos_pkg.vhd
+
+ghdl -a --ieee=synopsys --work=math_library %source%/math_library/state_variable/state_variable_pkg.vhd 
+ghdl -a --ieee=synopsys --work=math_library %source%/math_library/pi_controller/pi_controller_pkg.vhd
+
+ghdl -a --ieee=synopsys --work=math_library %source%/math_library/division/division_internal_pkg.vhd
+ghdl -a --ieee=synopsys --work=math_library %source%/math_library/division/division_pkg.vhd
+ghdl -a --ieee=synopsys --work=math_library %source%/math_library/division/division_pkg_body.vhd
+
 ghdl -a --ieee=synopsys tb_grid_inverter_control.vhd
 ghdl -e --ieee=synopsys tb_grid_inverter_control
 ghdl -r --ieee=synopsys tb_grid_inverter_control --vcd=tb_grid_inverter_control.vcd
